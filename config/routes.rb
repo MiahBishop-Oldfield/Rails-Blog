b74dd79061d683250de9820b5f30c5b23b27f863
Miah Bishop-Oldfield <73050296+MiahBishop-Oldfield@users.noreply.github.com>
2023-02-27 16:51:45 +1030

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  get 'dashboards/index', as: :dashboards
   resources :logins, only: %i[new create]
   resources :registrations, only: %i[new create]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   # root "articles#index"
   root 'posts#index'
   resources :posts
